be4e2f7219eff9819615b8eb59d5d66c66d6193b
Will Smith <willsmithte@gmail.com>
2024-02-23 10:50:48 -0800

    init comit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { blogs } from "~/blogs";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +9,18 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return { blogs };
+};
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <a href={`/blog/${encodeURIComponent(blog.title)}`}>{blog.title}</a>
+        </div>
+      ))}
     </div>
   );
 }
